23203ad70a8c2eccb7393fb0576910893ea14abb
Mohamed Adel <moadelwo77@gmail.com>
2025-07-26 17:00:54 +0300

    feat(user): implement user management endpoints and DTO validation
    - Updated user controller to handle CRUD operations for users.
    - Changed controller route from 'user' to 'users'.
    - Added methods to get all users, get a user by ID, create a user, update a user, and delete a user.
    - Introduced CreateUserDto with validation rules using class-validator.
    - Added class-validator dependency to package.json.

---
+++ api/src/user/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, IsEmail, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+    @IsString()
+    @IsNotEmpty()
+    @IsStrongPassword()
+    password: string
+}
--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,6 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
+    constructor (
+        private readonly userService: UserService 
+    ) {}
+
+    @Get()
+    getAllUsers(
+        @Query('skip') skip?: string,
+        @Query('take') take?: string,
+        @Query('cursor') cursor?: string,
+        @Query('where') where?: string,
+        @Query('orderBy') orderBy?: string,
+    ) {
+        return this.userService.users({
+            skip: skip ? +skip : undefined,
+            take: take ? +take : undefined,
+            cursor: cursor ? JSON.parse(cursor) : undefined,
+            where: where ? JSON.parse(where) : undefined,
+            orderBy: orderBy ? JSON.parse(orderBy) : undefined,
+        });
+    }
+
+    @Get(':id')
+    getUser(@Param('id') id: Number) {
+        this.userService.user({id: +id});
+    }
+
+    @Post()
+    createUser(@Body() data: CreateUserDto) {
+        this.userService.createUser(data);
+    }
+
+    @Patch('id')
+    updateUser(@Param('id') id: String, @Body() data: CreateUserDto) {
+        this.userService.updateUser({ 
+            where: {id: +id},
+            data 
+        });
+    }
     
+    @Delete('id')
+    deleteUser(@Param('id') id: String) {
+        this.userService.deleteUser({ id: +id })
+    }
 }
