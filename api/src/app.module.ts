836e953654aeebe639b8b9e3b7bfbff7e7258f7c
mohamedaalsayed <moadelwo77@gmail.com>
2025-08-05 18:52:36 +0300

    feat(item): implement item service to handle all item operations

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
